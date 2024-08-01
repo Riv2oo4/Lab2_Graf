fb6961813e4c1f84fdfb55b7517b3c90fc4d34a1
Riv2oo4 <riv22500@uvg.edu.gt>
2024-08-01 17:59:43 -0600

    Create main

---
+++ src/main.rs
@@ -0,0 +1,104 @@
+mod framebuffer;
+
+use minifb::{Window, WindowOptions};
+use std::time::Duration;
+use framebuffer::Framebuffer;
+
+fn main() {
+    let window_width = 800;
+    let window_height = 800;
+
+    let framebuffer_width = 100;
+    let framebuffer_height = 100;
+
+    let mut framebuffer = Framebuffer::new(framebuffer_width, framebuffer_height);
+
+    let mut window = Window::new(
+        "Conway's Game of Life",
+        window_width,
+        window_height,
+        WindowOptions::default(),
+    ).unwrap_or_else(|e| {
+        panic!("{}", e);
+    });
+
+    // Funciones para crear patrones
+    fn set_glider(state: &mut Vec<Vec<bool>>, x: isize, y: isize) {
+        let coords = [(x, y), (x + 1, y + 1), (x + 1, y + 2), (x, y + 2), (x - 1, y + 2)];
+        for &(ix, iy) in &coords {
+            if ix >= 0 && iy >= 0 && ix < state[0].len() as isize && iy < state.len() as isize {
+                state[iy as usize][ix as usize] = true;
+            }
+        }
+    }
+
+    fn set_lwss(state: &mut Vec<Vec<bool>>, x: isize, y: isize) {
+        let coords = [(x, y), (x + 3, y), (x + 4, y + 1), (x, y + 2), (x + 4, y + 2), (x + 1, y + 3), (x + 2, y + 3), (x + 3, y + 3)];
+        for &(ix, iy) in &coords {
+            if ix >= 0 && iy >= 0 && ix < state[0].len() as isize && iy < state.len() as isize {
+                state[iy as usize][ix as usize] = true;
+            }
+        }
+    }
+
+    fn set_block(state: &mut Vec<Vec<bool>>, x: isize, y: isize) {
+        let coords = [(x, y), (x + 1, y), (x, y + 1), (x + 1, y + 1)];
+        for &(ix, iy) in &coords {
+            if ix >= 0 && iy >= 0 && ix < state[0].len() as isize && iy < state.len() as isize {
+                state[iy as usize][ix as usize] = true;
+            }
+        }
+    }
+
+    fn set_blinker(state: &mut Vec<Vec<bool>>, x: isize, y: isize) {
+        let coords = [(x, y), (x, y + 1), (x, y + 2)];
+        for &(ix, iy) in &coords {
+            if ix >= 0 && iy >= 0 && ix < state[0].len() as isize && iy < state.len() as isize {
+                state[iy as usize][ix as usize] = true;
+            }
+        }
+    }
+
+    fn draw_spaceship(state: &mut Vec<Vec<bool>>, x: isize, y: isize) {
+        let coords = [
+            (x, y), (x + 1, y + 1), (x + 1, y + 2), (x, y + 2), (x - 1, y + 2),
+            (x + 2, y), (x + 2, y + 1), (x + 2, y + 2),
+        ];
+        for &(ix, iy) in &coords {
+            if ix >= 0 && iy >= 0 && ix < state[0].len() as isize && iy < state.len() as isize {
+                state[iy as usize][ix as usize] = true;
+            }
+        }
+    }
+
+    let mut state = vec![vec![false; framebuffer_width]; framebuffer_height];
+    let mut spaceship_x: isize = 0;
+    let spaceship_y: isize = framebuffer_height as isize / 2;
+    let mut spaceship_active = true;
+    
+    set_glider(&mut state, 10, 10);
+    set_lwss(&mut state, 20, 20);
+    set_block(&mut state, 30, 30);
+    set_blinker(&mut state, 40, 40);
+
+    while window.is_open() && !window.is_key_down(minifb::Key::Escape) {
+        framebuffer.clear();
+
+        if spaceship_active {
+            if spaceship_x < framebuffer_width as isize {
+                draw_spaceship(&mut state, spaceship_x, spaceship_y);
+                spaceship_x += 1;
+            } else {
+                spaceship_active = false;
+            }
+        } else {
+            framebuffer.render(&mut state);
+        }
+
+        window
+            .update_with_buffer(&framebuffer.buffer, framebuffer_width, framebuffer_height)
+            .unwrap();
+
+        std::thread::sleep(Duration::from_millis(50)); 
+    }
+}
